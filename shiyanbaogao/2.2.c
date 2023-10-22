07c91ce37a8b9299ad6aaf3149d58f96e58d053a
heifan <heifan0226@gmail.com>
2023-10-22 23:35:43 +0800

    massage

---
+++ shiyanbaogao/2.2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<windows.h>
+int main() 
+{
+	printf("****\n");
+	printf("*\n");
+	printf("*\n");
+	printf("****");
+    system("pause");
+}
---
+++ shiyanbaogao/2.5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<windows.h>
+int main()
+{
+int a,b,h,v;
+scanf("%d,%d,%d",&a,&b,&h);
+v=a*b*h;
+printf("长方体的体积V=%d\n",v);
+system("pause");
+return 0;
+}
